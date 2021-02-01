93e01699cd4035a9ecc636f46df40ceb08f332b3
ffedelefrsf <faustofedele2013@gmail.com>
2021-02-01 02:09:02 -0300

    Merge fix
    Merge fix

--- funesoft-app/src/app/app.component.ts
+++ funesoft-app/src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
 
-  constructor(private router: Router ) { }
+  constructor(public router: Router) { }
+  
   title = 'funesoft-app';
 }
